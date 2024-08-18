d793806a33df6c6744137f741753e8f72722c033
serhatcetin-kral <serhat06.sc@gmail.com>
2024-08-18 19:02:39 -0400

    first commit

---
+++ src/main/java/org/example/Dev.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+
+
+public class Dev {
+
+    public Dev(){
+        System.out.println("Dev class here");
+    }
+
+    public void build(){
+
+
+    System.out.println("Building Dev page ");
+}
+
+
+}
---
+++ src/main/java/org/example/Laptop.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Laptop {
+
+    public Laptop(){
+        System.out.println("Laptop constructor");
+    }
+
+    public void compiler(){
+        System.out.println("laptop class i have mac aseee");
+    }
+}
