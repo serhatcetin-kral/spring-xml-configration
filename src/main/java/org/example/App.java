aa72d1779a1e7b510d39c14d89c40ae206219fb3
serhatcetin-kral <serhat06.sc@gmail.com>
2024-08-18 21:19:52 -0400

    spring getter setter

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,5 @@
 package org.example;
 
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,8 +8,11 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-//        Dev obj=context.getBean(Dev.class);
-//        obj.build();
+      Dev obj=(Dev) context.getBean("dev");
+     //obj.age=18;
+      // obj.setAge(18);
+         System.out.println(obj.getAge());
+      //  obj.build();
 
 
     }
--- src/main/java/org/example/Dev.java
+++ src/main/java/org/example/Dev.java
@@ -4,7 +4,13 @@ package org.example;
 
 
 public class Dev {
+   private Laptop laptop;
+    private int age=8;
 
+    public Dev(Laptop laptop) {
+        this.laptop = laptop;
+        System.out.println("thsis is dev constructo for laptop");
+    }
     public Dev(){
         System.out.println("Dev class here");
     }
@@ -13,7 +19,27 @@ public class Dev {
 
 
     System.out.println("Building Dev page ");
+    laptop.compiler();
 }
 
+public Dev(int age){
+        this.age=age;
+        System.out.println("Dev 1 contructor");
+}
 
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
